dfc4d011ea550ff169bf87c3d76a11f1cda821c8
Teo <DAIca@gmail.com>
2022-07-14 10:32:17 +0700

    Second add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { PublicRouter } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
-  return (
-    <div className="App">
-        <h1>TIKTOK _ UI</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {PublicRouter.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/pages/Upload/index.js
@@ -0,0 +1,6 @@
+// import { HeaderOnly } from '~/components/Layout';
+function Upload() {
+    return <h2>Upload</h2>;
+}
+
+export default Upload;
